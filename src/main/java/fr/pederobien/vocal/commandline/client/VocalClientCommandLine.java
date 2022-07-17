1b0cd3ff060edbdc3ffb19cdf6bcf785559d0dd2
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2022-07-17 14:17:17 +0200

    vocal-client: Adding support of audio communication

--- src/main/java/fr/pederobien/vocal/commandline/client/VocalClientCommandLine.java
+++ src/main/java/fr/pederobien/vocal/commandline/client/VocalClientCommandLine.java
@@ -5,8 +5,10 @@ import fr.pederobien.commandline.CommandLine.CommandLineBuilder;
 import fr.pederobien.commandtree.events.NodeEvent;
 import fr.pederobien.communication.event.ConnectionEvent;
 import fr.pederobien.dictionary.event.DictionaryEvent;
-import fr.pederobien.sound.event.SoundEvent;
+import fr.pederobien.sound.event.ProjectSoundEvent;
 import fr.pederobien.utils.event.EventLogger;
+import fr.pederobien.vocal.client.event.VocalPlayerSpeakPostEvent;
+import fr.pederobien.vocal.client.event.VocalPlayerSpeakPreEvent;
 import fr.pederobien.vocal.commandline.client.impl.EVocalClientCode;
 import fr.pederobien.vocal.commandline.client.impl.VocalClientCommandTree;
 
@@ -22,7 +24,8 @@ public class VocalClientCommandLine {
 
 		CommandLineBuilder builder = new CommandLineBuilder(root -> {
 			EventLogger.instance().newLine(true).timeStamp(true).ignore(DictionaryEvent.class).ignore(ConnectionEvent.class);
-			EventLogger.instance().ignore(NodeEvent.class).ignore(SoundEvent.class).register();
+			EventLogger.instance().ignore(VocalPlayerSpeakPreEvent.class).ignore(VocalPlayerSpeakPostEvent.class);
+			EventLogger.instance().ignore(NodeEvent.class).ignore(ProjectSoundEvent.class).register();
 
 			String dictionaryFolder = commandLine.getEnvironment() == CommandLine.DEVELOPMENT_ENVIRONMENT ? DEV_DICTIONARY_FOLDER : PROD_DICTIONARY_FOLDER;
 			commandLine.registerDictionaries(dictionaryFolder, new String[] { "English.xml", "French.xml" });
